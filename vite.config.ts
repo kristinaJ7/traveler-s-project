e896eed2b42df222b18d616f3bcf278522b3f595
kristina filimonova <kristina.jk7@yandex.ru>
2026-01-22 16:15:57 +0300

    Fix GitHub Pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+   publicDir: 'public',  // копирует public/ в dist/public/
   plugins: [],
   css: {
     preprocessorOptions: {
